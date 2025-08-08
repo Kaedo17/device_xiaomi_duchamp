6c7d4c53ec6f499b1a1e8a0d23c0cbef36bc8c93
Kaedo17 <kurtandrieasoy17@gmail.com>
2025-08-08 00:35:09 +0000

    added overlay evolution jn device.mk

--- device.mk
+++ device.mk
@@ -206,6 +206,9 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-evolution
+
 PRODUCT_PACKAGES += \
    FrameworksResOverlayDuchamp \
    PowerOffAlarmOverlayDuchamp \
