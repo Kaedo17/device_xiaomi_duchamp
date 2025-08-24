8a5b1bf21f303f1ad21cb279b0ad3ad8368466b6
Kemji <kurtandrieasoy17@gmail.com>
2025-08-24 09:18:05 +0000

    duchamp: import htsr utils

--- parts/src/com/xiaomi/settings/touchsampling/TouchSamplingUtils.java
+++ parts/src/com/xiaomi/settings/touchsampling/TouchSamplingUtils.java
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package com.xiaomi.settings.touchsampling;
-
+package org.lineageos.settings.touchsampling;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -33,7 +31,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 public final class TouchSamplingUtils {
     private static final String TAG = "TouchSamplingUtils";
-    public static final String HTSR_FILE = "/sys/devices/platform/goodix_ts.0/switch_report_rate";
+    public static final String HTSR_FILE = "/sys/devices/platform/goodix_ts.0/goodix_ts_report_rate";
     public static void restoreSamplingValue(Context context) {
         SharedPreferences sharedPref = context.getSharedPreferences(
                 TouchSamplingSettingsFragment.SHAREDHTSR, Context.MODE_PRIVATE);
@@ -41,4 +39,3 @@ public final class TouchSamplingUtils {
         FileUtils.writeLine(HTSR_FILE, Integer.toString(htsrState));
     }
 }
-
