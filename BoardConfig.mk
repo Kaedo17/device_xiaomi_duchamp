a4cb569a43d979ec55e156de91c8d7ace81606dc
Kemji <kurtandrieasoy17@gmail.com>
2025-09-06 06:40:48 +0000

    Revert "duchamp:Drop duplicates & Adapt tree for hw/dolby"
    This reverts commit 640f12cd4974aae7b964712f4071e6d66a4d26b6.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/infinity/config/device_framework_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Kernel
